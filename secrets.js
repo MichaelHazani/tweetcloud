2d38acf636f967b900b79221ead2961a0a8dbcdc
Michael Hazani <123mickey@gmail.com>
2015-07-02 19:42:30 -0700

    rm gitignore for heroku

---
+++ secrets.js
@@ -0,0 +1,9 @@
+var keys = {
+  consumerKey:'Sac39Y6C6fJ3yAcX3xme80Hz7',
+  consumerSecret:'bHeBpGBFJFe66kRXcHeQWU6TKp6uiu7QO9wTCX83lw8B2lJMpM',
+  token:'189239506-XsfaRhqtPja2Qt9Xpc2CleLtNgvuxPYBMLA5nPWn',
+  tokenSecret:'Bdd4pRQNNEoCv1V4vtZyGyjyh4ke9wgbFSPe0YAIhjOxa'
+}
+
+
+module.exports = keys;
