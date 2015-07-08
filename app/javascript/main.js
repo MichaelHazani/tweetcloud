c15d258f73a860c2b40c6c1eb0c11b8f73b3343d
Michael Hazani <123mickey@gmail.com>
2015-07-08 14:57:11 -0700

    add sound

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -3,8 +3,9 @@
 'use strict';
 
   //threeJS
-  var scene, camera, renderer, plane, hemilight, sphere, pointCloud;
-  var controls;
+  var scene, camera, renderer, plane, pivot;
+  var controls, mouseX, mouseY;
+  var mouseDown = false;
 
 $(function(){
 
@@ -18,86 +19,17 @@ render();
 //THREE.js regular stuff
 
 function init() {
+
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 10000 );
-camera.position.x = 1;
+camera.position.z = -20;
 scene.add(camera);
 
 
-//light
-// var hemiLight = new THREE.HemisphereLight(0xffffff, 0x00ff00, 8);
-// hemiLight.position.set(0, 10, 0);
-// scene.add(hemiLight);
-
-// var pointLight = new THREE.PointLight(0xFFFFFF, 9);
-// pointLight.position.set(0, 10, 0);
-
-// scene.add(pointLight);
 
 //OrbitControls
 controls = new THREE.OrbitControls(camera);
 controls.damping = 0.2;
-
-//canvas experiment
-// var myCanvas = document.createElement('canvas');
-// var ctx = myCanvas.getContext('2d');
-// //ctx.clearRect(0,0,myCanvas.width,myCanvas.height);
-
-// ctx.font = "30px courier new";
-// ctx.fillStyle="rgba(255,0,0,0.95)";
-// var text = "Now is the Winter of Our Discontent";
-// //ctx.font = "courier new";
-// // ctx.rect(0,0,256,256);
-// // ctx.fillStyle="black";
-// // ctx.fill();
-// // ctx.fillStyle="pink";
-// ctx.fillText(text, 30, 128);
-
-// ctx.textAlign = "center";
-// var canText1 = new THREE.Texture(myCanvas);
-// canText1.needsUpdate = true;
-
-
-// //test plane
-//   var geometry = new THREE.PlaneGeometry(myCanvas.width, myCanvas.height);
-
-// //try canvas
-// // var canMat = new THREE.MeshBasicMaterial( {map:canText1, side:THREE.DoubleSide});
-// // canMat.transparent = true;
-// //canMat.transparent = true;
-
-// //test pointCloud
-// var random = Math.floor(Math.random()*(max - min) + min);
-// var vertex;
-// var cloudGeo = new THREE.Geometry();
-// for (var i = 0; i < 1300; i++) {
-//   var vertexX = Math.floor(Math.random()*(max - min) + min),
-//   vertexY = Math.floor(Math.random()*(max - min) + min),
-//   vertexZ = Math.floor(Math.random()*(max - min) + min),
-//   particle = new THREE.Vector3( vertexX, vertexY, vertexZ );
-//   cloudGeo.vertices.push(particle);
-//   // console.log(cloudGeo.vertices);
-// }
-
-// var cloudMat = new THREE.PointCloudMaterial( {color:0xff00ff, map:canText1, size:5, side:THREE.DoubleSide} );
-
-// pointCloud = new THREE.PointCloud(cloudGeo, cloudMat);
-// //pointCloud.sortParticles = true;
-// pointCloud.position.set(0,0,0);
-// scene.add(pointCloud);
-
-
-// reg. material
-
-  var material = new THREE.MeshPhongMaterial( {
-                                              color: 0xFF0080,
-                                              transparent: true,
-                                              opacity: 0.5 } );
-  // plane = new THREE.Mesh( geometry, canMat );
-  // plane.position.set(0,20,0);
-  // scene.add( plane );
-
-
 
 
 renderer = new THREE.WebGLRenderer( {antialias: true, alpha:true} );
@@ -107,6 +39,7 @@ renderer.setClearColor(0x000000);
 }
 
 
+
 //window resize
 window.addEventListener('resize', function () {
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -114,18 +47,90 @@ camera.aspect = window.innerWidth / window.innerHeight;
 camera.updateProjectionMatrix();
 });
 
+//event listeners and mouse/touch reaction, courtesy of:
+//http://codepen.io/seanseansean/pen/EaBZEY?editors=001
+// document.addEventListener('mousemove', onDocumentMouseMove, false);
+// document.addEventListener('touchstart', onDocumentTouchStart, false);
+// document.addEventListener('touchmove', onDocumentTouchMove, false);
+// var  windowHalfX = window.innerWidth / 2;
+// var  windowHalfY = window.innerHeight / 2;
+// function onDocumentMouseMove(e) {
+//   if (!mouseDown) {
+//   mouseX = e.clientX - windowHalfX;
+//   mouseY = e.clientY - windowHalfY;
+// }
+// }
+
+// function onDocumentTouchStart(e) {
+
+//   if (e.touches.length === 1) {
+
+//     e.preventDefault();
+//     mouseX = e.touches[0].pageX - windowHalfX;
+//     mouseY = e.touches[0].pageY - windowHalfY;
+//   }
+// }
+
+// function onDocumentTouchMove(e) {
+
+//   if (e.touches.length === 1) {
+
+//     e.preventDefault();
+//     mouseX = e.touches[ 0 ].pageX - windowHalfX;
+//     mouseY = e.touches[ 0 ].pageY - windowHalfY;
+//   }
+// }
+
+//don't confuse shmancy auto-move with OrbitControls
+// $(document).mousedown(function(){mouseDown = true; console.log("mouseDown")});
+// $(document).mouseup(function(){mouseDown = false; console.log("mouseUp")});
+
+$(document).keydown(function(e) {
+   console.log(e);
+
+  switch(e.keyCode) {
+    case 83: //down
+      camera.translateZ( + 4 );
+      break;
+    case 87: //up
+      camera.translateZ( - 4 );
+      break;
+    case 65: //left
+      camera.rotation.x -= 4;
+    break;
+    case 68: //right
+      camera.rotation.x += 4;
+      break;
+
+  }
+  /* Act on the event */
+});
 
 function render() {
-
+var time = Date.now() * 0.0000005;
 requestAnimationFrame(render);
-renderer.render(scene, camera);
-// pointCloud.rotation.y+=0.001;
-// pointCloud.rotation.z+=0.001;
-// pointCloud.rotation.x-=0.001;
 
 
+//mouse-sensitive cam movement
+// camera.position.x += (mouseX - camera.position.x) * 0.09;
+// camera.position.y += (- mouseY - camera.position.y) * 0.09;
+// camera.lookAt(scene.position);
+
+
+//organic rotation
+for (var i = 0; i < scene.children.length; i ++) {
+
+    var object = scene.children[i];
+
+      object.rotation.y = time * (i < 4 ? i + 1 : - (i + 1));
+  }
+//update OrbitControls
 controls.update();
+renderer.render(scene, camera);
+
+
 }
+
 
 
 
