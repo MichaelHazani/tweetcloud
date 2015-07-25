d6a39cfcdfb436f65c7898552a0b7e72ac2a8f62
Michael Hazani <123mickey@gmail.com>
2015-07-25 15:42:14 -0700

    fix tweet cutoff, change max tweets

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ var bodyparser = require('body-parser');
 var tweetList;
 
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 
 
 app.use(express.static('app'));
@@ -26,9 +28,9 @@ app.use(express.static('app'));
 
 server.listen(process.env.PORT || 3000);
 
-var twitterStreamClient =  new twitter.StreamClient(
-  keys.consumerKey, keys.consumerSecret, keys.token, keys.tokenSecret
-  );
+var twitterStreamClient = new twitter.StreamClient(
+    keys.consumerKey, keys.consumerSecret, keys.token, keys.tokenSecret
+);
 twitterStreamClient.on('close', function() {
     console.log('Connection closed.');
 });
@@ -44,52 +46,52 @@ twitterStreamClient.on('error', function(error) {
 //   // var thisTweet = tweet.text;
 //   // var thisTime = tweet.time;
 //   tweets.push(tweet.text);
-  // console.log(tweets);
-  // socket.emit("twitter-stream", extTweet);
-  //send out to web sockets channel
+// console.log(tweets);
+// socket.emit("twitter-stream", extTweet);
+//send out to web sockets channel
 
 // });
 
 
 app.get('/', function(req, res) {
-  res.sendFile('index.html');
+    res.sendFile('index.html');
 });
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function(socket) {
 
-socket.on("start tweets", function(data) {
-  twitterStreamClient.start([data]);
-  //socket.emit('twitter-stream', tweets);
-  });
+    socket.on("start tweets", function(data) {
+        twitterStreamClient.start([data]);
+        //socket.emit('twitter-stream', tweets);
+    });
 
-socket.on('stop', function(){
-twitterStreamClient.stop();
-tweets = [];
-socket.disconnect();
-});
+    socket.on('stop', function() {
+        twitterStreamClient.stop();
+        tweets = [];
+        socket.disconnect();
+    });
 
-socket.on('disconnect', function(){
-twitterStreamClient.stop();
-tweets = [];
-socket.disconnect();
-// socket.close();
-});
+    socket.on('disconnect', function() {
+        twitterStreamClient.stop();
+        tweets = [];
+        socket.disconnect();
+        // socket.close();
+    });
 
 
-twitterStreamClient.on('tweet', function(tweet) {
-  // tweeting = true;
-  // var thisTweet = tweet.text;
-  // var thisTime = tweet.time;
-  // for (var i = 0; i < 100; i++) {
-  //   console.log(i + tweet.text);
-    tweets.push(tweet.text);
-    //console.log(tweets);
-    socket.emit("twitter-stream", tweets);
-    tweets = [];
-    // send out to web sockets channel
-  // }
-    //twitterStreamClient.stop();
-});
+    twitterStreamClient.on('tweet', function(tweet) {
+        // tweeting = true;
+        // var thisTweet = tweet.text;
+        // var thisTime = tweet.time;
+        // for (var i = 0; i < 100; i++) {
+        //   console.log(i + tweet.text);
+        tweets.push(tweet.text);
+        //console.log(tweets);
+        socket.emit("twitter-stream", tweets);
+        tweets = [];
+        // send out to web sockets channel
+        // }
+        //twitterStreamClient.stop();
+    });
 
 });
 
@@ -101,8 +103,3 @@ twitterStreamClient.on('tweet', function(tweet) {
 //   console.log(tweetList);
 //   res.json(tweetList);
 // });
-
-
-
-
-
